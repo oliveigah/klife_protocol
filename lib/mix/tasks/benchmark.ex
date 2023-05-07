d1c318478c8427e0adc08274b7ede64bebf00ac6
oliveigah <oliveira.gahenrique@gmail.com>
2023-05-07 03:41:43 -0300

    chore: prepare for benchmarks

--- lib/mix/tasks/benchmark.ex
+++ lib/mix/tasks/benchmark.ex
@@ -1,106 +1,17 @@
-if Mix.env() == :dev do
+if Mix.env() in [:dev] do
   defmodule Mix.Tasks.Benchmark do
     alias KlifeProtocol.Serializer
+    alias KlifeProtocol.TestSupport.Helpers
+    alias KlifeProtocol.Messages
     use Mix.Task
 
     def run(args) do
+      Helpers.initialize_shared_storage()
       do_run_bench(List.first(args))
     end
 
-    defp do_run_bench("crc32c") do
-      input = :rand.bytes(10000)
-
-      Benchee.run(
-        %{
-          "elixir_crc" => fn -> KlifeProtocol.CRC32c.execute(input) end,
-          "nif_crc" => fn -> :crc32cer.nif(input) end
-        },
-        time: 10,
-        memory_time: 2
-      )
-    end
-
-    defp do_run_bench("create_topic_serialization") do
-      Benchee.run(
-        %{
-          "klife_protocol" => fn ->
-            KlifeProtocol.Messages.CreateTopics.serialize_request(
-              %{
-                headers: %{correlation_id: 1357, client_id: "some_crazy_client"},
-                content: %{
-                  topics: [
-                    %{
-                      name: "my_first_topic_abc_new_2",
-                      num_partitions: 3,
-                      replication_factor: 2,
-                      assignments: [],
-                      configs: []
-                    }
-                  ],
-                  timeout_ms: 2000,
-                  validate_only: true
-                }
-              },
-              0
-            )
-          end,
-          "kafka_protocol" => fn ->
-            :kpro_req_lib.create_topics(
-              0,
-              [
-                %{
-                  name: "my_first_topic_abc_new_2",
-                  num_partitions: 3,
-                  replication_factor: 2,
-                  assignments: [],
-                  configs: []
-                }
-              ],
-              %{timeout: 2000, validate_only: true}
-            )
-          end
-        },
-        time: 10,
-        memory_time: 2
-      )
-    end
-
-    defp do_run_bench("varint_serialization") do
-      input = -512
-
-      Benchee.run(
-        %{
-          "klife" => fn -> Serializer.execute(input, :varint) end,
-          "varint_lib" => fn ->
-            input
-            |> Varint.Zigzag.encode()
-            |> Varint.LEB128.encode()
-          end
-        },
-        time: 10,
-        memory_time: 2
-      )
-    end
-
-    defp do_run_bench("unsigned_varint_serialization") do
-      input = 300
-
-      Benchee.run(
-        %{
-          "klife" => fn -> Serializer.execute(input, :unsigned_varint) end,
-          "varint_lib" => fn -> Varint.LEB128.encode(input) end
-        },
-        time: 10,
-        memory_time: 2
-      )
-    end
-
-    defp do_run_bench("records_serialization") do
-      headers = %{
-        correlation_id: 12345,
-        client_id: "klife_benchmark"
-      }
-
+    defp do_run_bench("produce_serialization") do
+      headers = Helpers.genereate_headers()
       ts = DateTime.to_unix(DateTime.utc_now())
 
       generate_content = fn qty, size ->
@@ -145,13 +56,70 @@ if Mix.env() == :dev do
         }
       end
 
-      input = %{headers: headers, content: generate_content.(1, 100_000)}
+      input = %{headers: headers, content: generate_content.(1, 10_000)}
 
       Benchee.run(
         %{
-          "test" => fn ->
-            KlifeProtocol.Messages.Produce.serialize_request(input, 0)
-          end
+          "test" => fn -> Messages.Produce.serialize_request(input, 0) end
+        },
+        time: 10,
+        memory_time: 2
+      )
+    end
+
+    defp do_run_bench("fetch_deserialization") do
+      version = Messages.Fetch.max_supported_version()
+      topic_name = "benchmark_topic"
+      {:ok, topic_id} = Helpers.get_or_create_topic(topic_name)
+
+      produce_content = fn qty, size ->
+        Enum.map(1..qty, fn _ -> :rand.bytes(10_000) end)
+      end
+
+      {:ok,
+       %{
+         broker: broker,
+         partition_index: partition_index,
+         offset: offset_1
+       }} = Helpers.produce_message(topic_name, produce_content.(1, 10_000))
+
+      headers = Helpers.genereate_headers()
+
+      content = %{
+        replica_id: -1,
+        max_wait_ms: 1000,
+        min_bytes: 1,
+        max_bytes: 1_000_000,
+        isolation_level: 0,
+        session_id: 0,
+        session_epoch: 0,
+        topics: [
+          %{
+            topic_id: topic_id,
+            partitions: [
+              %{
+                partition: partition_index,
+                current_leader_epoch: 0,
+                fetch_offset: offset_1,
+                last_fetched_epoch: 0,
+                log_start_offset: 0,
+                partition_max_bytes: 1_000_000
+              }
+            ]
+          }
+        ],
+        forgotten_topics_data: [],
+        rack_id: "rack"
+      }
+
+      binary_input =
+        %{headers: headers, content: content}
+        |> Messages.Fetch.serialize_request(version)
+        |> Helpers.send_message_to_broker(broker)
+
+      Benchee.run(
+        %{
+          "test" => fn -> Messages.Fetch.deserialize_response(binary_input, version) end
         },
         time: 10,
         memory_time: 2
