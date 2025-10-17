1dfe2642323b52e1abfcfb959f26fb2533ac82c5
Gabriel Oliveira <oliveira.gahenrique@gmail.com>
2025-10-17 01:33:05 -0300

    wip: perf test files

--- lib/klife_protocol.ex
+++ lib/klife_protocol.ex
@@ -2,4 +2,14 @@ defmodule KlifeProtocol do
   @moduledoc """
   Use the `KlifeProtocol.Messages` modules to serialze and deserialize messages.
   """
+
+  def perf_test() do
+    bin = File.read!("test/files/large_fetch_resp")
+
+    t0 = System.monotonic_time(:millisecond)
+    {:ok, _resp} = KlifeProtocol.Messages.Fetch.deserialize_response(bin, 4)
+    tf = System.monotonic_time(:millisecond)
+
+    tf - t0
+  end
 end
--- run-kafka.sh
+++ run-kafka.sh
@@ -2,4 +2,4 @@
 VERSION=${1:-"latest"}
 
 bash ./stop-kafka.sh "$VERSION"
-docker-compose -f ./test/compose_files/docker-compose-kafka-${VERSION}.yml up --force-recreate
+docker compose -f ./test/compose_files/docker-compose-kafka-${VERSION}.yml up --force-recreate
