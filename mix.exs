0efade310d924c3ed86c6dd34f1dd71f1c442671
Gabriel Oliveira <oliveira.gahenrique@gmail.com>
2026-07-05 13:44:25 -0300

    chore: bump release

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule KlifeProtocol.MixProject do
   def project do
     [
       app: :klife_protocol,
-      version: "0.10.0",
+      version: "1.0.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
